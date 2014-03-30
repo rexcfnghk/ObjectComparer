204036ed1405687a78163a54234098352eb886a2
rexcfnghk <rexcfnghk@gmail.com>
2014-03-30 21:29:44 +0800

    Commit missing PrimitivesHolder

---
+++ Voyagers.Utilities.ObjectComparer.Tests/TestClasses/PrimitivesHolder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Voyagers.Utilities.ObjectComparer.Tests.TestClasses
+{
+    public class PrimitivesHolder
+    {
+        public int Int1 { get; set; }
+
+        public char Char1 { get; set; }
+
+        public IEnumerable<string> Strings { get; set; }
+    }
+}
