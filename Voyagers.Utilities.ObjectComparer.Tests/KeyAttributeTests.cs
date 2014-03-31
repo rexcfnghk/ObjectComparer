ea961bc23f0aa7f8943bfe9da387bb965e171c52
rexcfnghk <rexcfnghk@gmail.com>
2014-03-31 22:15:51 +0800

    Implemented checking for KeyAttribute for a given type

---
+++ Voyagers.Utilities.ObjectComparer.Tests/KeyAttributeTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Voyagers.Utilities.ObjectComparer.Tests.TestClasses;
+using Xunit;
+
+namespace Voyagers.Utilities.ObjectComparer.Tests
+{
+    public class KeyAttributeTests
+    {
+        [Fact]
+        public void UserContainingKeyAttributeCanBeDetected()
+        {
+            // Arrange
+            IEnumerable<PropertyInfo> propertyInfos;
+
+            // Act
+            ObjectComparer.TryGetKeyAttriubte(typeof(User), out propertyInfos);
+
+            // Assert
+            var infos = propertyInfos as IList<PropertyInfo> ?? propertyInfos.ToList();
+            Assert.NotEmpty(infos);
+            Assert.Equal(1, infos.Count());
+            Assert.Equal("Id", infos[0].Name);
+        }
+
+        [Fact]
+        public void ClassContainingTwoKeysCanBeDetected()
+        {
+            // Arrange
+            IEnumerable<PropertyInfo> propertyInfos;
+
+            // Act
+            ObjectComparer.TryGetKeyAttriubte(typeof(ClassWithTwoKeys), out propertyInfos);
+
+            // Assert
+            var infos = propertyInfos as IList<PropertyInfo> ?? propertyInfos.ToList();
+            Assert.NotEmpty(infos);
+            Assert.Equal(2, infos.Count());
+            Assert.Equal("Id", infos[0].Name);
+            Assert.Equal("Name", infos[1].Name);
+        }
+    }
+}
---
+++ Voyagers.Utilities.ObjectComparer.Tests/TestClasses/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Voyagers.Utilities.ObjectComparer.Tests.TestClasses
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
