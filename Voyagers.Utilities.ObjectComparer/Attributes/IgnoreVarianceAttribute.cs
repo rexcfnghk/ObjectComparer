9370f2f8e3dbe4b84d9979d472ced9435a3b725e
Rex Ng <rexcfnghk@gmail.com>
2014-04-03 14:10:05 +0800

    Seal IgnoreVarianceAttribute to make lookup faster

--- Voyagers.Utilities.ObjectComparer/Attributes/IgnoreVarianceAttribute.cs
+++ Voyagers.Utilities.ObjectComparer/Attributes/IgnoreVarianceAttribute.cs
@@ -3,7 +3,7 @@
 namespace Voyagers.Utilities.ObjectComparer.Attributes
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-    public class IgnoreVarianceAttribute : Attribute
+    public sealed class IgnoreVarianceAttribute : Attribute
     {
     }
 }
