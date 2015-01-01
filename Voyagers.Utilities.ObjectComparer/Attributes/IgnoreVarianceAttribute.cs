9b865eae390dc49689d8f2948c17a6f0a373a6fc
rexcfnghk <rexcfnghk@gmail.com>
2015-01-01 16:36:03 +0800

    Remove redundant property settings

--- Voyagers.Utilities.ObjectComparer/Attributes/IgnoreVarianceAttribute.cs
+++ Voyagers.Utilities.ObjectComparer/Attributes/IgnoreVarianceAttribute.cs
@@ -5,7 +5,7 @@ namespace Voyagers.Utilities.ObjectComparer.Attributes
     /// <summary>
     /// Attribute that instructs <c>ObjectComparer</c> to ignore variances on a property or the whole class 
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, Inherited = false)]
     public sealed class IgnoreVarianceAttribute : Attribute
     {
     }
