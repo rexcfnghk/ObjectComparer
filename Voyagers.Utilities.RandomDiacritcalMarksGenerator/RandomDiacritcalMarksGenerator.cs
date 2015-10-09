8cc137f41d1b8ea44fbb6e8a4c4fd6d3a13920ed
Rex Ng <rexng@thisisnoble.com>
2015-10-09 16:50:01 +0800

    Add a project to generate diacritcal marks

---
+++ Voyagers.Utilities.RandomDiacritcalMarksGenerator/RandomDiacritcalMarksGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Voyagers.Utilities.RandomDiacritcalMarksGenerator
+{
+    public static class RandomDiacritcalMarksGenerator
+    {
+        private const int _rangeLowest = 768;
+        private const int _rangeHighest = 879;
+
+        public static string Abuse(this string input, int threshold)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            var stringBuilder = new StringBuilder();
+            var random = new Random();
+            TextElementEnumerator textElementEnumerator = StringInfo.GetTextElementEnumerator(input);
+            while (textElementEnumerator.MoveNext())
+            {
+                int numberOfDiacriticalMarks = random.Next(-1, threshold) + 1;
+
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private static string AddCombiningDiacritics(string input, int number)
+        {
+            var stringBuilder = new StringBuilder(input, input.Length + number);
+            var random = new Random();
+            for (int i = 0; i < number; i++)
+            {
+                byte randomDiacrtic = Convert.ToByte(random.Next(_rangeLowest - 1, _rangeHighest) + 1);
+                stringBuilder.Append(Encoding.Unicode.GetString(new[] { randomDiacrtic }));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
